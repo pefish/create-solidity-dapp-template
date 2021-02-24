00d2c6842b9f5c75b0c0e9dc98fb9d1908799943
pefish <pefish@qq.com>
2021-02-24 17:28:52 +0800

    add

--- migrations/1_deploy_test_contract.ts
+++ migrations/1_deploy_test_contract.ts
@@ -1,7 +1,9 @@
+const Proxy = artifacts.require("Proxy");
 const Test = artifacts.require("Test");
 
 module.exports = async function (deployer) {
   await deployer.deploy(Test);
+  deployer.deploy(Proxy, Test.address);
 } as Truffle.Migration;
 
 export {};
