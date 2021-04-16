8e071b31948885e45931c63c2e3d855fcc3f4d61
pefish <pefish@qq.com>
2021-04-16 17:13:57 +0800

    add

--- migrations/1_deploy_test_contract.ts
+++ migrations/1_deploy_test_contract.ts
@@ -4,7 +4,7 @@ import Web3 from "web3"
 
 declare var web3: Web3;
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer,network, accounts) {
   await deployer.deploy(Test);
 
   // deploy proxy
@@ -13,17 +13,15 @@ module.exports = async function (deployer) {
   const proxyAddress = Proxy.address
   // @ts-ignore
   let proxyContract: web3.eth.Contract = new web3.eth.Contract(Proxy.abi, proxyAddress)
-  const owner = await proxyContract.methods["owner"]().call()
-
   // set implement
   await proxyContract.methods["upgradeTo"](Test.address).send({
-    from: owner,
+    from: accounts[0],
   })
 
   // init
   let proxyContract1 = new web3.eth.Contract(Test.abi, proxyAddress)
   await proxyContract1.methods["init"](123).send({
-    from: owner,
+    from: accounts[0],
   })
 } as Truffle.Migration;
 
