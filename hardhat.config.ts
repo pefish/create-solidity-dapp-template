c83d8d9e0774f18447e9408eec5402c47a946e18
pefish <darrondarron@qq.com>
2023-07-17 15:05:49 +0800

    add

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,54 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "arbitrum",
   networks: {
     hardhat: {
+      zksync: false,
     },
-    goerli: {
-      url: "https://eth-goerli.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
-      accounts: [process.env.PKEY || "0000000000000000000000000000000000000000000000000000000000000000"]
+    eth: {
+      url: "https://rpc.ankr.com/eth",
+      accounts: [process.env.PKEY || "0000000000000000000000000000000000000000000000000000000000000000"],
+      chainId: 1,
+      zksync: false,
     },
-    arbitrum: {
+    ethTestnet: {
+      url: "https://rpc.ankr.com/eth_goerli",
+      accounts: [process.env.PKEY || "0000000000000000000000000000000000000000000000000000000000000000"],
+      chainId: 5,
+      zksync: false,
+    },
+    zk: {
+      url: "https://mainnet.era.zksync.io",
+      ethNetwork: "",
+      chainId: 324,
+      zksync: true,
+      verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
+      timeout: 10000
+    },
+    zkTestnet: {
+      url: "https://zksync2-testnet.zksync.dev", // https://goerli-api.zksync.io/jsrpc
+      ethNetwork: "",
+      chainId: 280,
+      zksync: true,
+      timeout: 10000
+    },
+    arb: {
       url: "https://arb1.arbitrum.io/rpc",
       accounts: [process.env.PKEY || "0000000000000000000000000000000000000000000000000000000000000000"],
       chainId: 42161,
+      zksync: false,
+    },
+    arbTestnet: {
+      url: "https://goerli-rollup.arbitrum.io/rpc",
+      accounts: [process.env.PKEY || "0000000000000000000000000000000000000000000000000000000000000000"],
+      chainId: 421613,
+      zksync: false,
     }
   },
   solidity: {
